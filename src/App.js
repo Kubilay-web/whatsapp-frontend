d605c2f1444ef2d451f1cf6631e7e6366056d25c
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2024-05-17 19:08:25 +0300

    Updated

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { io } from "socket.io-client";
+import SocketContext from "./context/SocketContext";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+
+function App() {
+  //const [connected, setConnected] = useState(false);
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+  const { token } = user;
+
+  const socket = io(process.env.REACT_APP_API_ENDPOINT.split("/api/v1")[0]);
+  return (
+    <div className="dark">
+      <SocketContext.Provider value={socket}>
+        <Router>
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                token ? <Home socket={socket} /> : <Navigate to="/login" />
+              }
+            />
+            <Route
+              exact
+              path="/login"
+              element={!token ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              exact
+              path="/register"
+              element={!token ? <Register /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </Router>
+      </SocketContext.Provider>
+    </div>
+  );
+}
+
+export default App;
